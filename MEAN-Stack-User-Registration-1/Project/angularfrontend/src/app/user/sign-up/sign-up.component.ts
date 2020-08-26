0b861c37c5680dd10bc0fcb119e86d6c719c5b1a
GaneshDesk <koradeganesh.r@gmail.com>
2020-08-26 14:40:26 +0530

    User registration + MongoDB

---
+++ MEAN-Stack-User-Registration-1/Project/angularfrontend/src/app/user/sign-up/sign-up.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/user.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css'],
+})
+export class SignUpComponent implements OnInit {
+  public Name: string;
+  public mail: string;
+  public pass: string; 
+
+  public emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  public showSucessMessage: boolean;
+  public serverErrorMessages: string;
+  
+
+  constructor(private userService: UserService) {
+    this.Name=this.userService.selectedUser.fullName;
+    this.mail=this.userService.selectedUser.email;
+    this.pass=this.userService.selectedUser.password;
+    
+   }
+
+  ngOnInit(): void {
+  }
+  onSubmit(form: NgForm) {
+    this.userService.postUser(form.value).subscribe(
+      res => {
+        this.showSucessMessage = true;
+        setTimeout(() => this.showSucessMessage = false, 4000);
+        this.resetForm(form);
+      },
+      err => {
+        if (err.status === 422) {
+          this.serverErrorMessages = err.error.join('<br/>');
+        }
+        else
+          this.serverErrorMessages = 'Something went wrong.Please contact admin.';
+      }
+    );
+  }
+
+  resetForm(form: NgForm) {
+    this.userService.selectedUser = {
+      fullName: '',
+      email: '',
+      password: ''
+    };
+    form.resetForm();
+    this.serverErrorMessages = '';
+  }
+
+}
